f3185c5104606958363329155a4ad1e25aca15a5
daniellimadev <pdaniel643@gmail.com>
2023-10-25 12:04:01 -0300

    Starting the project and configuring kafka config

---
+++ producer/src/test/java/com/study/daniel/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.daniel.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
